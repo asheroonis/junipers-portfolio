72477546a83e04bd6315d02f46bfb208e2c151b3
Blake Robinson <brob952@aucklanduni.ac.nz>
2024-09-10 17:02:32 +1200

    Completed frontend

--- src/initial-projects.js
+++ src/initial-projects.js
@@ -5,7 +5,7 @@ export const initialProjects = [
         previewDescription : "A 50-person story-centered Minecraft server run on a custom modpack.",
         dateCompleted : "October 2023 - January 2024",
         languagesUsed : "Java-Based Scripting",
-        fullDescription : "Full description will be inserted here",
+        fullDescription : "This project was quite the exercise in leadership and development skills. When I found that simply playing modded Minecraft wasn't enough, I decided to look into creating my own modpack. I went through an iterative process, adding mods to the pack and creating custom scripts to add custom quests and to bridge any gaps between the mods such as duplicate item recipes, etc. before testing them with friends and refining the pack each time. When I finished the pack after about a month, I started a publicly-accessible server with the modpack in which players could join and create their own nation, join an existing one or go it alone. The idea of the server was centered around roleplay and storytelling, with frequent seasonal events that my staff team and I organised for the players. I found myself having to work out a lot of drama between players on the server, being a surprising exercise in conflict resolution. Ultimately the server shut down when I started to get ready for the university year and did not have time to run it, but it was still a fantastic experience I won't forget.",
         displayLeft : true,
         previewImageLink : "/soralia/soralia_server_1.png",
         fullImageLinks : ["/soralia/soralia_server_1.png","/soralia/soralia_server_2.png","/soralia/soralia_server_3.png","/soralia/soralia_server_4.png","/soralia/soralia_server_5.png","/soralia/soralia_server_6.png","/soralia/soralia_server_7.png","/soralia/soralia_server_8.png","/soralia/soralia_server_9.png","/soralia/soralia_server_10.png","/soralia/soralia_server_11.png"]
@@ -16,7 +16,7 @@ export const initialProjects = [
         previewDescription : "Digitally designing my own custom Lego sets from scratch and getting familiar with the design and iterative processes.",
         dateCompleted : "August 2022 - Present",
         languagesUsed : "N/A",
-        fullDescription : "Full description will be inserted here",
+        fullDescription : "I've always been a major Lego fan ever since I was little, and over the past few years I have found a great creative outlet in the form of digital Lego design. I use a program called Bricklink Studio which has a database of all the available pieces and colours and use it to construct various models as you can see here. For the larger projects, I often have multiple attempts at creating the finalised design, starting with one rough draft to get the general picture and then further refining it - the images which feature multiple trains next to each other would be examples of these. I share some of my designs that I particularly like on my instagram page (@bricks.by.june) and I hope some day to source the parts and build them in real life, but until then they are stuck just existing in my computer!",
         displayLeft : false,
         previewImageLink : "/lego/lego_design_1.png",
         fullImageLinks : ["/lego/lego_design_1.png","/lego/lego_design_2.png","/lego/lego_design_3.png","/lego/lego_design_4.png","/lego/lego_design_5.png","/lego/lego_design_6.png","/lego/lego_design_7.png","/lego/lego_design_8.png","/lego/lego_design_9.png","/lego/lego_design_10.png","/lego/lego_design_11.png","/lego/lego_design_12.png","/lego/lego_design_13.png","/lego/lego_design_14.png","/lego/lego_design_15.png","/lego/lego_design_16.png","/lego/lego_design_17.png","/lego/lego_design_18.png"]
@@ -27,7 +27,7 @@ export const initialProjects = [
         previewDescription : "Writing lore and notes as part of a large team making a huge mod for the game Slime Rancher.",
         dateCompleted : "April 2020 - August 2020",
         languagesUsed : "N/A",
-        fullDescription : "Full description will be inserted here",
+        fullDescription : "My first proper experience working as part of a large team, I was a writer for the Viktor's Diskoveries Mod (or VikDisk for short). This was a mod led by RickTheCoder that planned to overhaul the game Slime Rancher and essentially double the size of the game with a lot more content. Over the course of development, I was part of a 4-person writing team and helped to write the lore notes that the player would walk around and find to slowly piece together the story arc that we had constructed. We had regular schedules for our writing tasks and frequently had to meet deadlines to keep the mod's development on track, and ultimately everything ran quite smoothly. Unfortunately in the height of the pandemic, the primary leaders of the project found themselves needing to focus on their day jobs to maintain a liveable income and could not make time for the mod's development. Shortly after, the project's progress stopped and sadly did not end up seeing the light of day, but it was still an extremely valuable experience for all of us involved.",
         displayLeft : true,
         previewImageLink : "/vikdisk/vikdisk_mod_1.png",
         fullImageLinks : ["/vikdisk/vikdisk_mod_1.png","/vikdisk/vikdisk_mod_2.png","/vikdisk/vikdisk_mod_3.png"]
